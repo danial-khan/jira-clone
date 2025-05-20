4bb3a44286821f4336f74e5be7ca7c0570291372
danial-khan <danial.devsol@gmail.com>
2025-05-20 17:49:33 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}", // if you're using /src
+    "./src/features/**/*.{js,ts,jsx,tsx,mdx}", 
+  ],
+  theme: {
+    extend: {
+      colors: {
+        destructive: "#ef4444",
+        "destructive-foreground": "#ffffff",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+}
